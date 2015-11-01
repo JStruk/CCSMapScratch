cf1944e69a83b704d53fc500dbc4bd6ea2f14147
Justin <strukjustin@gmail.com>
2015-11-01 18:47:51 -0500

    :)

--- android/src/com/mygdx/game/android/AndroidLauncher.java
+++ android/src/com/mygdx/game/android/AndroidLauncher.java
@@ -4,13 +4,13 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.mygdx.game.CCStouchpad;
+import com.mygdx.game.CCSMap;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new CCStouchpad(), config);
+		initialize(new CCSMap(), config);
 	}
 }
